09078481dde6223f06d7fa90a3631fa2294be4a7
IrishBruse <econn50@outlook.com>
2024-07-23 00:58:42 +0100

    Remove yaml

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # My VSCode cross platform binding generator
+
+https://github.com/codebling/vs-code-default-keybindings - 1.91.1
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,66 @@
 import * as YAML from "yaml";
 import * as fs from "fs";
 
-function Generate(negative: string, output: string) {
+type OS = "linux" | "macos" | "windows";
+
+let useWindowsAsBase = true;
+let useCustomKeybinds = true;
+let useExtenstionNegativeKeybinds = true;
+
+function Generate(negative: string, os: OS) {
   let negativeKeybinds: Keybind[] = JSON.parse(
     fs.readFileSync(negative).toString()
   );
 
-  let windowsKeybinds: Keybind[] = JSON.parse(
-    fs.readFileSync("defaultKeybinds/windows.keybindings.json").toString()
-  );
+  const keybinds = [...negativeKeybinds];
 
-  let customKeybinds = YAML.parse(fs.readFileSync("keybinds.yaml").toString());
+  if (useWindowsAsBase) {
+    keybinds.push(...loadConfig("defaultKeybinds/windows.keybindings.json"));
+  }
 
-  const keybinds = [...negativeKeybinds, ...windowsKeybinds, ...customKeybinds];
+  if (useCustomKeybinds) {
+    const customKeybinds = loadConfig("custom.json").map((v) => {
+      return {
+        key: convertKey(v.key, os),
+        when: v.when,
+        command: v.command,
+        args: v.args,
+      };
+    });
 
-  fs.writeFileSync(output, JSON.stringify(keybinds, null, 4));
+    keybinds.push(...customKeybinds);
+  }
+
+  if (useExtenstionNegativeKeybinds) {
+    keybinds.push(
+      ...loadConfig("defaultKeybinds/extensions.negative.keybinds.json")
+    );
+  }
+
+  if (os === "linux") {
+    fs.writeFileSync(
+      "/home/econn/.config/Code/User/keybindings.json",
+      JSON.stringify(keybinds, null, 4)
+    );
+  }
+
+  fs.writeFileSync(os + ".keyboard.json", JSON.stringify(keybinds, null, 4));
 }
 
-Generate(
-  "defaultKeybinds/linux.negative.keybindings.json",
-  "linux.keyboard.json"
-);
-Generate(
-  "defaultKeybinds/macos.negative.keybindings.json",
-  "macos.keyboard.json"
-);
+function convertKey(key: string, platform: OS) {
+  if (platform === "macos") {
+    return key.replace("ctrl+", "cmd+");
+  }
+  return key;
+}
+
+function loadConfig(path: string): Keybind[] {
+  return JSON.parse(fs.readFileSync(path).toString());
+}
+
+Generate("defaultKeybinds/linux.negative.keybindings.json", "linux");
+Generate("defaultKeybinds/macos.negative.keybindings.json", "macos");
+Generate("defaultKeybinds/windows.negative.keybindings.json", "windows");
 
 type Keybind = {
   key: string;
